193b19d71707f74cdeb70da475b8b29f7c2ec4d1
Rolly <77501604+NekoRolly@users.noreply.github.com>
2026-06-30 14:02:55 -0500

    Remplazar DP por Ad Hoc

--- Olimpiadas/OII/OII_2026/C3_17p.cpp
+++ Olimpiadas/OII/OII_2026/C3_17p.cpp
@@ -1,16 +1,23 @@
 // Complejidad  : O(n)
 // Memoria      : O(n)
 // Puntaje      : 0 + 7 + 10 + 0 + 0 + 0 = 17
-// Idea         : DP
+// Idea         : Ad Hoc
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef pair<ll,ll> pll;
 const int N = 2e6+4;
 const ll infll = 1e18+4;
 
 int n,k;
 int h[N],a[N];
-ll dp1[N],dp2[N];
+ll dp[N];
+ll pr_a[N],pr_h[N];
+
+ll Agua(int l,int r){
+    if (l > r) return 0;
+    return (pr_a[r] - pr_a[l-1]) - (pr_h[r] - pr_h[l-1]);
+}
 
 void solve(){
     cin >> n >> k;
@@ -25,14 +32,20 @@ void solve(){
     for (int i=n; i>=1; i--)
         mx = max(mx, h[i]), a[i] = min(a[i], mx);
 
-    dp1[1] = 0, dp2[1] = infll;
-    for (int i=2; i<=n; i++){
-        dp1[i] = min(dp1[i-1], dp2[i-1]) + a[i] - h[i];
-        if (a[i] == h[i]) dp2[i] = infll;
-        else dp2[i] = dp1[i-1] + a[i] - h[i] - 1;
+    vector<int> vec;
+    for (int i=1; i<=n; i++){
+        pr_a[i] = pr_a[i-1] + a[i];
+        pr_h[i] = pr_h[i-1] + h[i];
+        if (a[i] == h[i]) vec.push_back(i);
     }
 
-    cout << dp1[n] << '\n';
+    ll ans = 0;
+    for (int i=1; i<vec.size(); i++){
+        ll A = Agua(vec[i-1]+1, vec[i]-1);
+        int len = vec[i] - vec[i-1] - 1;
+        ans += A - (len+1)/2;
+    }
+    cout << ans << "\n";
 }
 
 int main(){
